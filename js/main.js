899dddd5bff15bd1ccb5d78a2e340a6b7925117a
bugti <harryston23@gmail.com>
2020-11-04 11:54:20 +0100

    deleting comment

--- js/main.js
+++ js/main.js
@@ -42,31 +42,3 @@ var DataModel = Backbone.Model.extend({});
   });
 
   view.render();
-
-
-
-
-
-
-
-
-/*let myChart = document.getElementById('myChart').getContext("2d");
-
-let Sale = new Chart(myChart, {
-    type: "line",
-    data:{
-        labels:[ 1980, 1985, 1990,  1995, 2000],
-        datasets:[{
-            label:"Sales",
-            data:[
-             14000, 
-             22000,
-            104000, 
-             87000, 
-             43400 
-            ],
-            
-        }]
-    },
-    options:{}
-});*/
